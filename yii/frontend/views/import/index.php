5955a9a29cb77c55704de7e66aa11007d6583149
Yaroslav <goobar4.699@gmail.com>
2020-10-19 13:37:31 +0200

    sql

--- yii/frontend/views/import/index.php
+++ yii/frontend/views/import/index.php
@@ -27,6 +27,5 @@ use yii\helpers\Url;
 <?php ActiveForm::end() ?>
 
 <p>The file for import should be saved in csv format with UTF 8 encoding. Please use this
-<?= Html::a('example', Url::base($schema = true).'/uploads/test.csv')?>
-  as a template.
-Use ; as separtor and do not use ; for any other purposes. The format of the date should be yyyy-mm-dd.</p>
+<a href="#" download= <?= Url::base($schema = true).'/uploads/test.csv'?> title="">example</a>
+a template. Use ; as separtor and do not use ; for any other purposes. The format of the date should be yyyy-mm-dd.</p>
